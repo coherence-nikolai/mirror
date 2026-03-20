6f0a58ce3891c86d1bacdbe98dfc78994590ef07
coherence-nikolai <nhalay@gmail.com>
2026-03-21 00:15:15 +1100

    Add files via upload

--- components/mirror/MirrorScreen.tsx
+++ components/mirror/MirrorScreen.tsx
@@ -126,6 +126,7 @@ export default function MirrorScreen() {
 
   const headerRight = (
     <>
+      <Link href="/step" className="btn">Step</Link>
       <Link href="/witness" className="btn">Witness</Link>
       <Link href="/breath" className="btn">Breath</Link>
       <Link href="/voice" className="btn">Voice</Link>
